26a76f0e38a0b8fe129068cf72163cc1cb2fe725
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-08-14 12:39:49 +0300

    refactoring multiple-lines-path

--- src/acf/parser.clj
+++ src/acf/parser.clj
@@ -1,24 +1,26 @@
 (ns acf.parser)
 
-(def multiple-lines-file "test/multiline.acf")
+(def multiple-lines-path "test/multiline.acf")
 
 (defn read-file [filename]
   (slurp filename))
-
 (defn split-by-newline [s]
   (clojure.string/split s #"\n"))
+
 (defn read-by-line [filename]
   (split-by-newline
    (read-file filename)))
+
+(def multiple-lines-file (read-by-line multiple-lines-path))
+(comment multiple-lines-file)
 
 (defn split-by-whitespace [s]
   (clojure.string/split s #"\t+"))
 
 (defn trim-key-value [s]
-  (clojure.string/trim
-
-
-   s))
+  
+(clojure.string/trim
+ s))
 
 (defn remove-quotes [s]
   (clojure.string/replace
@@ -28,8 +30,7 @@
 (defn readkeyvalue [s]
   (split-by-whitespace
    (remove-quotes
-    (trim-key-value
-     s))))
+    (trim-key-value s))))
 
 (defn keywordize-things [[key value & other]]
   (hash-map
@@ -38,11 +39,24 @@
 (comment (read-by-line
           multiple-lines-file))
 
+(map (fn [unmapped-row]
+       (map vector vamp-keys unmapped-row))
+     rows)
+
+(defn clean-lines [lines]
+  (reduce into
+          (map #(readkeyvalue %)
+               lines)))
+
+(comment (clean-lines
+          multiple-lines-file))
+
 (defn parse-by-line [multiple-lines]
   (reduce into
           (map #(keywordize-things (readkeyvalue %))
                multiple-lines)))
 
+(comment (:installdir (parse-by-line
+                       multiple-lines-file)))
 (defn -main [& args]
-  (:installdir (parse-by-line
-                (read-by-line multiple-lines-file))))
+  (:installdir (parse-by-line multiple-lines-file)))
