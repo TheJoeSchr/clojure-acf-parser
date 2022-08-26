4e78f857e682f3dc201fa56e56099357b3ffbae1
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-08-26 16:44:38 +0300

    parser: add main, read list of files, prints installdirs

--- src/acf/parser.clj
+++ src/acf/parser.clj
@@ -1,33 +1,32 @@
-(ns acf.parser)
+(ns acf.parser (:require [babashka.fs :as fs]
+                         [clojure.edn :as edn]
+                         [clojure.java.io :as io]))
 
-(def multiple-lines-path "test/multiline.acf")
+(def multiple-lines-path "../../test/multiline.acf")
+(def full-acf-path "../../test/example.acf")
 
-(defn read-file [filename]
-  (slurp filename))
+(comment
+  (fs/exists? full-acf-path))
 
 (defn split-by-newline [s]
   (clojure.string/split s #"\n"))
 
 (defn read-by-line [filename]
   (split-by-newline
-   (read-file filename)))
+   (slurp filename)))
 
-(def multiple-lines-file (read-by-line multiple-lines-path))
-(comment multiple-lines-file)
+(defn read-file [filename]
+  (when (fs/exists? filename)
+    (read-by-line filename)))
 
 (defn split-by-whitespace [s]
   (clojure.string/split s #"\t+"))
 
 (defn trim-key-value [s]
-
-  (clojure.string/trim
-
-   s))
-
+  (clojure.string/trim s))
+(comment (trim-key-value "\t asdf \t"))
 (defn remove-quotes [s]
-  (clojure.string/replace
-   s #"\\|\""
-   {"\\" "" "\"" ""}))
+  (clojure.string/replace s #"\\|\"" {"\\" "" "\"" ""}))
 
 (defn readkeyvalue [s]
   (split-by-whitespace
@@ -35,27 +34,62 @@
     (trim-key-value s))))
 
 (defn clean-lines [lines]
-  (map #(readkeyvalue %) lines))
+  (map
+   #(readkeyvalue %) lines))
 
-(comment (clean-lines multiple-lines-file))
 
-(defn keywordize-things [[key value & other]]
+(defn 
+keywordize-things [[key value & other]]
   (hash-map
    (keyword key) value))
+
+(defn mapify-lines [lines]
+  (map #(keywordize-things %)
+       lines))
+
+(comment (keywordize-things
+          ["appid" "1059980"]))
+
+
+(comment (first (clean-lines multiple-lines-file)))
+(comment
+  (keywordize-things (first (clean-lines multiple-lines-file))))
+(comment (mapify-lines
+          (clean-lines
+           multiple-lines-file)))
+
+(defn parse-by-line [lines]
+  (reduce into
+          (mapify-lines (clean-lines
+                         lines))))
+
+
+(def multiple-lines-file (read-file multiple-lines-path))
+(def acf-file (read-file full-acf-path))
+(comment (parse-by-line multiple-lines-file))
+(comment (:installdir (parse-by-line multiple-lines-file)))
 
 ; (map (fn [unmapped-row]
 ;        (map vector vamp-keys unmapped-row))
 ;      rows)
+(comment (parse-by-line acf-file))
+(comment
+  (:installdir
+   (parse-by-line
+    acf-file)))
 
-(defn mapify-lines [lines]
-  (map #(keywordize-things %) lines))
+(defn mapInstalldir [filenames] (map #(:installdir (parse-by-line (read-file %))) filenames))
+(def -filenames '("../../files/appmanifest_1190460.acf" "../../files/appmanifest_12210.acf" "../../files/appmanifest_1282730.acf" "../../files/appmanifest_1434950.acf"))
+(comment (mapInstalldir filenames))
 
-(defn parse-by-line [lines]
-  (reduce into
-          (mapify-lines
-           (clean-lines lines))))
-
-(comment (:installdir (parse-by-line
-                       multiple-lines-file)))
 (defn -main [& args]
-  (:installdir (parse-by-line multiple-lines-file)))
+  (do
+    (comment filenames)
+
+    ; (println (fs/exists? multiple-lines-path))
+    ; (println "-main: done")
+    ; (println (:installdir (parse-by-line multiple-lines-file)))
+    ; (edn/read *in*)
+    (let [filenames (line-seq
+                     (io/reader *in*))]
+      (mapInstalldir filenames))))
