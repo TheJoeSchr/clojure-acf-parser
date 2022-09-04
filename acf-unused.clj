892e0cb2496203e7143d0c20c53636ac341e828c
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-09-04 16:21:27 +0200

    put parser stuff into own ns and load ns via load-files

--- acf-unused.clj
+++ acf-unused.clj
@@ -2,100 +2,10 @@
 (ns acf.unused (:require [babashka.fs :as fs]
                          [clojure.edn :as edn]
                          [clojure.java.io :as io]))
-
-(def multiple-lines-path "test/multiline.acf")
-(def full-acf-path "test/example.acf")
-
-(comment
-  (fs/exists? full-acf-path))
-
-(defn split-by-newline [s]
-  (clojure.string/split
-   s #"\n"))
-
-(defn read-by-line [filename]
-  (split-by-newline
-   (slurp filename)))
-
-(defn read-file [filename]
-  (when (fs/exists? filename)
-    (read-by-line filename)))
-
-(defn split-by-whitespace [s]
-  (clojure.string/split s #"\t+"))
-
-(defn trim-key-value [s]
-  (clojure.string/trim s))
-
-(comment (trim-key-value "\t asdf \t"))
-(defn remove-quotes [s]
-  (clojure.string/replace
-   s #"\\|\"" {"\\" "" "\"" ""}))
-
-(defn readkeyvalue [s]
-  (split-by-whitespace
-   (remove-quotes
-    (trim-key-value s))))
-
-(defn clean-lines [lines]
-  (map
-   #(readkeyvalue %) lines))
-
-
-(defn keywordize-things [[key value & other]]
-  (hash-map
-   (keyword key) value))
-
-(defn mapify-lines [lines]
-  (map #(keywordize-things %)
-
-       lines))
-
-(comment (keywordize-things
-          ["appid" "1059980"]))
-
-
-(comment (first (clean-lines multiple-lines-file)))
-(comment
-  (keywordize-things (first (clean-lines 
-multiple-lines-file))))
-(comment (mapify-lines
-          (clean-lines
-           multiple-lines-file)))
-
-(defn parse-by-line [lines]
-  (reduce into
-          (mapify-lines (clean-lines
-                         lines))))
-
-(def multiple-lines-file (read-file multiple-lines-path))
-(def acf-file (read-file full-acf-path))
-(comment (parse-by-line multiple-lines-file))
-(comment (:installdir (parse-by-line multiple-lines-file)))
-
-; (map (fn [unmapped-row]
-;        (map vector vamp-keys unmapped-row))
-;      rows)
-(comment (parse-by-line acf-file))
-(comment
-  (:installdir
-   (parse-by-line
-    acf-file)))
-
-(defn parse-file [file]
-  (parse-by-line
-   (read-file file)))
-
-
-(defn resolve-installs
-  [filenames commonpath]
-  (reduce
-   (fn [files file]
-    (let [installdir (:installdir (parse-file file))
-          exists (fs/exists? (str commonpath installdir))]
-           (into files [{:path file :installDir installdir :exists exists}])))
-             ()
-             filenames))
+(load-file "src/acf/parser.clj")
+(comment (keys (ns-publics *ns*)))
+(require '[acf.parser :as parser ])
+(comment (keys (ns-publics 'acf.parser)))
 
 (defn filterGames
   "looksup acf-files installdir inside commonfolder and filters accordingly to `shouldExists`"
@@ -103,20 +13,16 @@ multiple-lines-file))))
   (map :path
        (filter
         (fn [{exists :exists}] (= exists shouldExists))
-        (resolve-installs filenames commonpath))))
-
+        (parser/resolve-installs filenames commonpath))))
 
 (comment
-  (do
-    (defn mapInstalldir [filenames] (map #(:installdir (parse-file %)) filenames))
-
-(def -filenames '("test/files/appmanifest_1190460.acf" "test/files/appmanifest_12210.acf" "test/files/appmanifest_1282730.acf" "test/files/appmanifest_1434950.acf")))
-  (mapInstalldir -filenames)
-  (resolve-installs -filenames "test/files/common/")
-  (filterGames -filenames "test/files/common/" false)
-  (doseq [item (filterGames -filenames "test/files/common/" false)]
-   (println item))
-  (doseq (vec )))
+  (do   
+    (def -filenames '("test/files/appmanifest_1190460.acf" "test/files/appmanifest_12210.acf" "test/files/appmanifest_1282730.acf" "test/files/appmanifest_1434950.acf"))
+      (resolve-installs -filenames "test/files/common/")
+      (filterGames -filenames "test/files/common/" false)
+      (doseq [item (filterGames -filenames "test/files/common/" false)]
+               (println item))
+      ) )
 
 (defn -main [& args]
   (let [filenames (line-seq (io/reader *in*))
@@ -127,13 +33,13 @@ multiple-lines-file))))
       ; adding second arg prints debug info
       (if (> (count args) 1)
         (do (println (str "common:  " commonpath))
-          (println (str "# files: " (count filenames)))
-          (println (str "# games found: " (count games-found)))
-          (println (str "# games not found: " (count games-not-found)))))
+            (println (str "# files: " (count filenames)))
+            (println (str "# games found: " (count games-found)))
+            (println (str "# games not found: " (count games-not-found)))))
       ; only print missing games if we at least found one
       (if (< (count games-not-found) (count filenames))
         (doseq [acf-file games-not-found]
-         (println acf-file ))
+          (println acf-file ))
         ))))
 
 ; helps running file directly via bb -f
---
+++ src/acf/parser.clj
@@ -0,0 +1,105 @@
+#!/usr/bin/env bb
+(ns acf.parser (:require [babashka.fs :as fs]
+                         [clojure.edn :as edn]
+                         [clojure.java.io :as io]))
+
+(def multiple-lines-path "test/multiline.acf")
+(def full-acf-path "test/example.acf")
+
+(comment
+  (fs/exists? full-acf-path))
+
+(defn split-by-newline [s]
+  (clojure.string/split
+   s #"\n"))
+
+(defn read-by-line [filename]
+  (split-by-newline
+   (slurp filename)))
+
+(defn read-file [filename]
+  (when (fs/exists? filename)
+    (read-by-line filename)))
+
+(defn split-by-whitespace [s]
+  (clojure.string/split s #"\t+"))
+
+(defn trim-key-value [s]
+  (clojure.string/trim s))
+
+(comment (trim-key-value "\t asdf \t"))
+(defn remove-quotes [s]
+  (clojure.string/replace
+   s #"\\|\"" {"\\" "" "\"" ""}))
+
+(defn readkeyvalue [s]
+  (split-by-whitespace
+   (remove-quotes
+    (trim-key-value s))))
+
+(defn clean-lines [lines]
+  (map
+   #(readkeyvalue %) lines))
+
+
+(defn keywordize-things [[key value & other]]
+  (hash-map
+   (keyword key) value))
+
+(defn mapify-lines [lines]
+  (map #(keywordize-things %)
+
+       lines))
+
+(comment (keywordize-things
+          ["appid" "1059980"]))
+
+
+(comment (first (clean-lines multiple-lines-file)))
+(comment
+  (keywordize-things (first (clean-lines 
+multiple-lines-file))))
+(comment (mapify-lines
+          (clean-lines
+           multiple-lines-file)))
+
+(defn parse-by-line [lines]
+  (reduce into
+          (mapify-lines (clean-lines
+                         lines))))
+
+(def multiple-lines-file (read-file multiple-lines-path))
+(def acf-file (read-file full-acf-path))
+(comment (parse-by-line multiple-lines-file))
+(comment (:installdir (parse-by-line multiple-lines-file)))
+
+; (map (fn [unmapped-row]
+;        (map vector vamp-keys unmapped-row))
+;      rows)
+(comment (parse-by-line acf-file))
+(comment
+  (:installdir
+   (parse-by-line
+    acf-file)))
+
+(defn parse-file [file]
+  (parse-by-line
+   (read-file file)))
+
+
+(defn resolve-installs
+  [filenames commonpath]
+  (reduce
+   (fn [files file]
+    (let [installdir (:installdir (parse-file file))
+          exists (fs/exists? (str commonpath installdir))]
+           (into files [{:path file :installDir installdir :exists exists}])))
+             ()
+             filenames))
+
+(comment 
+  (let [-filenames '("test/files/appmanifest_1190460.acf" "test/files/appmanifest_12210.acf" "test/files/appmanifest_1282730.acf" "test/files/appmanifest_1434950.acf")]
+    (comment (do 
+               (defn mapInstalldir [filenames] (map #(:installdir (parse-file %)) filenames))
+               (mapInstalldir -filenames) ))
+    (resolve-installs -filenames "common") ))
